db27a5a3dc183bdaafc015c287635e9ef6145a09
Mahmoud A. Gawad <mahmoudabdelgawad993@gmail.com>
2020-04-27 15:43:46 +0200

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ navigator.serviceWorker.register("/worker.js");
 
 function doFetch() {
   fetch("/test").then(response => {
-    console.log("Response:", response);
+    console.log("Response:", response.body);
   }).catch(err => {
     console.error("Error while fetching:", err);
   });
